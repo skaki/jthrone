5b9f39cb867c363fd5c2f77313775f99488d6d3d
Sudhakar Kaki <skaki@sudhakars-mbp.attlocal.net>
2017-10-06 08:16:08 -0700

    remove principal

--- src/main/java/com/thinkhr/api/controllers/DocumentController.java
+++ src/main/java/com/thinkhr/api/controllers/DocumentController.java
@@ -10,8 +10,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.security.Principal;
-
 /**
  * Created by skaki on 10/4/17.
  */
@@ -24,23 +22,20 @@ public class DocumentController {
     @GetMapping(path="/documents")
     @PreAuthorize("hasAuthority('ADMIN_USER')")
     public @ResponseBody
-    Iterable<Document> getAllUsers(Principal principal) {
-        System.console().writer().print(principal);
+    Iterable<Document> getAllUsers() {
         return docRepo.findAll();
     }
 
     @GetMapping("document/{id}")
     @PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
-    public ResponseEntity<Document> getById(@PathVariable(name="id",value = "id") Long id, Principal principal) {
-        // System.console().writer().print(principal);
+    public ResponseEntity<Document> getById(@PathVariable(name="id",value = "id") Long id) {
         Document document = docRepo.findOne(id);
         return new ResponseEntity<Document>(document, HttpStatus.OK);
     }
 
     @PostMapping("document")
     @PreAuthorize("hasAuthority('ADMIN_USER')")
-    public ResponseEntity<Void> create(@RequestBody Document document, UriComponentsBuilder builder, Principal principal) {
-        System.console().writer().print(principal);
+    public ResponseEntity<Void> create(@RequestBody Document document, UriComponentsBuilder builder) {
         docRepo.save(document);
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(builder.path("/document/{id}").buildAndExpand(document.getId()).toUri());
